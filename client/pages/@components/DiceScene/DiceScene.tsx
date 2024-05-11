13625d89f43031df0ecea6b2c6f1958be962680b
1F10230014 <s1f102300144@iniad.org>
2024-05-12 04:08:11 +0900

    update:さいころ回る機能追加※毎回あらし

--- client/pages/@components/DiceScene/DiceScene.tsx
+++ client/pages/@components/DiceScene/DiceScene.tsx
@@ -1,12 +1,16 @@
 import { useFBX } from '@react-three/drei';
 import { useLoader } from '@react-three/fiber';
 import { motion } from 'framer-motion-3d';
+import type { AnimationState } from 'pages/DiceApp/index.page';
 import React, { useRef, useState } from 'react';
 import { TextureLoader, type Mesh } from 'three';
 
-type BoxProps = {
+type DiceProps = {
   position: [x: number, y: number, z: number];
   diceValue: number;
+  animationState: AnimationState;
+  onClickDice: () => { newDiceValue: number };
+  startAnimation: () => void;
 };
 
 const diceValuePosition = [
@@ -18,31 +22,98 @@ const diceValuePosition = [
   { x: 0, y: 0.5 * Math.PI, z: -1 }, //6
 ];
 
-export const Dice: React.FC<BoxProps> = ({ diceValue, ...restProps }) => {
+export const Dice: React.FC<DiceProps> = ({
+  diceValue,
+  onClickDice,
+  startAnimation,
+  animationState,
+  ...restProps
+}) => {
   const group = useFBX('/models/dice/dice.fbx');
   const texture = useLoader(TextureLoader, '/models/dice/dice.png');
   const mesh = useRef<Mesh>(group.children[0] as Mesh);
   const geometry = mesh.current.geometry;
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
+  const [hovered, setHover] = useState<boolean>(false);
+  let newDiceValue: number = diceValue;
 
   return (
     <motion.mesh
       {...restProps}
       ref={mesh}
-      scale={active ? 1 : 0.5}
+      scale={hovered ? 0.7 : 0.5}
       geometry={geometry}
+      onClick={(): void => {
+        if (animationState === 'rest') {
+          const clickedResult = onClickDice();
+          setHover(false);
+          startAnimation();
+          newDiceValue = clickedResult.newDiceValue;
+        }
+      }}
       rotation={[
-        diceValuePosition[diceValue - 1].x,
-        diceValuePosition[diceValue - 1].y,
-        diceValuePosition[diceValue - 1].z,
+        diceValuePosition[newDiceValue - 1].x,
+        diceValuePosition[newDiceValue - 1].y,
+        diceValuePosition[newDiceValue - 1].z,
       ]}
-      onClick={() => setActive(!active)}
-      onPointerOver={() => setHover(true)}
+      animate={animationState}
+      onPointerOver={() => {
+        if (animationState === 'rest') {
+          setHover(true);
+        }
+      }}
       onPointerOut={() => setHover(false)}
+      variants={{
+        rest: {
+          y: 0,
+          rotateX: diceValuePosition[diceValue - 1].x,
+          rotateY: diceValuePosition[diceValue - 1].y,
+          rotateZ: diceValuePosition[diceValue - 1].z,
+        },
+        rolling: {
+          y: 2.5,
+          rotateX: diceValuePosition[newDiceValue - 1].x + 2 * Math.PI,
+          rotateY: diceValuePosition[newDiceValue - 1].y + 2 * Math.PI,
+          rotateZ: diceValuePosition[newDiceValue - 1].z + 2 * Math.PI,
+          transition: {
+            y: {
+              ease: 'circOut',
+            },
+            rotateX: {
+              duration: 0.2,
+              ease: 'linear',
+              repeat: Infinity,
+            },
+            rotateY: {
+              duration: 0.1,
+              ease: 'linear',
+              repeat: Infinity,
+            },
+            rotateZ: {
+              duration: 0.3,
+              ease: 'linear',
+              repeat: Infinity,
+            },
+          },
+        },
+        drop: {
+          y: -1,
+          rotateX: diceValuePosition[diceValue - 1].x,
+          rotateY: diceValuePosition[diceValue - 1].y,
+          rotateZ: diceValuePosition[diceValue - 1].z,
+          transition: {
+            rotation: {
+              duration: 0.1,
+            },
+            y: {
+              duration: 0.5,
+              ease: 'circIn',
+            },
+          },
+        },
+      }}
     >
-      {/* colorは6進数指定じゃないとバグる */}
-      <meshPhysicalMaterial map={texture} color={hovered ? '#64f351' : '#c3c2c2'} />
+      {/* colorは6進数指定でないとバグる */}
+      <meshPhysicalMaterial map={texture} color={hovered ? '#c6c6c6' : '#a8a8a8'} />
     </motion.mesh>
   );
 };
--- client/pages/DiceApp/index.page.tsx
+++ client/pages/DiceApp/index.page.tsx
@@ -6,13 +6,32 @@ import { BasicHeader } from 'pages/@components/BasicHeader/BasicHeader';
 import { Dice } from 'pages/@components/DiceScene/DiceScene';
 import { useState } from 'react';
 
+export type AnimationState = 'rest' | 'rolling' | 'drop';
+
 const DideApp = () => {
   const [user] = useAtom(userAtom);
   const [dicevalue, setDiceValue] = useState<number>(1);
+  const [animatinState, setAnimationState] = useState<AnimationState>('rest');
 
-  const clickDice = () => {
+  const sleep = (ms: number): Promise<void> => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  };
+
+  const startAnimation = async (): Promise<void> => {
+    setAnimationState('rolling');
+    await sleep(1000);
+    setAnimationState('drop');
+    await sleep(500);
+    setAnimationState('rest');
+  };
+
+  const onClickDice = (): { newDiceValue: number } => {
     const newDiceValue = Math.floor(Math.random() * 6 + 1);
     setDiceValue(newDiceValue);
+
+    return { newDiceValue };
   };
   if (!user) return <Loading visible />;
   return (
@@ -22,9 +41,27 @@ const DideApp = () => {
         <Canvas>
           <ambientLight intensity={1} />
           <pointLight position={[0, 10, 0]} intensity={200} />
-          <Dice position={[-3, 0, 0]} diceValue={dicevalue} />
-          <Dice position={[0, 0, 0]} diceValue={dicevalue} />
-          <Dice position={[3, 0, 0]} diceValue={dicevalue} />
+          <Dice
+            position={[-3, 0, 0]}
+            diceValue={dicevalue}
+            onClickDice={onClickDice}
+            startAnimation={startAnimation}
+            animationState={animatinState}
+          />
+          <Dice
+            position={[0, 0, 0]}
+            diceValue={dicevalue}
+            onClickDice={onClickDice}
+            startAnimation={startAnimation}
+            animationState={animatinState}
+          />
+          <Dice
+            position={[3, 0, 0]}
+            diceValue={dicevalue}
+            onClickDice={onClickDice}
+            startAnimation={startAnimation}
+            animationState={animatinState}
+          />
         </Canvas>
       </div>
     </>

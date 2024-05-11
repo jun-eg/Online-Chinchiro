c0558911df3a24ccc8c078e54e10f6e7fc5ffa2d
1F10230014 <s1f102300144@iniad.org>
2024-05-12 02:15:29 +0900

    update:light関連修正

--- client/pages/DiceApp/index.page.tsx
+++ client/pages/DiceApp/index.page.tsx
@@ -9,15 +9,22 @@ import { useState } from 'react';
 const DideApp = () => {
   const [user] = useAtom(userAtom);
   const [dicevalue, setDiceValue] = useState<number>(1);
+
+  const clickDice = () => {
+    const newDiceValue = Math.floor(Math.random() * 6 + 1);
+    setDiceValue(newDiceValue);
+  };
   if (!user) return <Loading visible />;
   return (
     <>
       <BasicHeader user={user} />
       <div style={{ width: '100vw', height: '100vh' }}>
         <Canvas>
-          <ambientLight />
-          <pointLight position={[0, 10, 0]} />
+          <ambientLight intensity={1} />
+          <pointLight position={[0, 10, 0]} intensity={200} />
+          <Dice position={[-3, 0, 0]} diceValue={dicevalue} />
           <Dice position={[0, 0, 0]} diceValue={dicevalue} />
+          <Dice position={[3, 0, 0]} diceValue={dicevalue} />
         </Canvas>
       </div>
     </>
